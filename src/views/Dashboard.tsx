c91fd14e49a00c8e2d7fa6b6cc7a1a99181c7bd4
cosmickitten <tjankui@gmail.com>
2022-07-15 16:48:39 +0200

    ✨ add db.json for json-server

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,15 +1,46 @@
 
+import { useEffect, useState } from 'react'
 import { Card, Column, Row } from '../components'
-import status from '../data/status.json'
-import tickets from '../data/tickets.json'
+
 
 const Dashboard = () => {
+  const [status, setStatus]=useState([]);
+  const [tickets, setTickets] = useState([]);
+  const [error, setError] = useState({})
+
+  useEffect(() => {
+    fetch('http://localhost:8000/status')
+    .then(res=>{
+      return res.json(); //asynchronous
+    })
+    .then(dataStatus => {
+      setStatus(dataStatus)
+    })
+    .catch(err => setError(err))
+  }, [])
+
+  interface statusProps{
+    id:number;
+    value:string
+  }
+  interface ticketProps{
+    id:number;
+    title:string;
+    status: number;
+    description:string
+  }
+  
+
   return (
     <div className='dashboard'>
       <Row>
-        {status.map((s)=>
-        <Column key={s.id} title={s.value} count={tickets.filter((t)=>s.id === t.status).length}>          
-          {tickets.filter((t)=>s.id === t.status).map((ticket)=> <Card key={ticket.id} {...ticket} />)}
+        {status.map((s:statusProps)=>
+        <Column 
+          key={s.id} 
+          title={s.value} 
+          count={tickets.filter((t:ticketProps)=>s.id === t.status).length}
+        >          
+          {tickets.length > 0 && tickets.filter((t:ticketProps)=>s.id === t.status).map((ticket:ticketProps)=> <Card key={ticket.id} {...ticket} />)}
         </Column>
         )}
       </Row>
