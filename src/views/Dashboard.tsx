92b3f80f2b8ae24ef9a48c2584cbfd6066b69307
cosmickitten <tjankui@gmail.com>
2022-07-15 18:24:26 +0200

    add useFetch hook

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,23 +1,11 @@
 
 import { useEffect, useState } from 'react'
-import { Card, Column, Row } from '../components'
+import { Row, List } from '../components'
+import useFetch from '../hooks/useFetch';
 
 
 const Dashboard = () => {
-  const [status, setStatus]=useState([]);
-  const [tickets, setTickets] = useState([]);
-  const [error, setError] = useState({})
-
-  useEffect(() => {
-    fetch('http://localhost:8000/status')
-    .then(res=>{
-      return res.json(); //asynchronous
-    })
-    .then(dataStatus => {
-      setStatus(dataStatus)
-    })
-    .catch(err => setError(err))
-  }, [])
+  const {data: dataStatuses, isLoading, error} = useFetch("https://my-json-server.typicode.com/shakkamakka/data/status")
 
   interface statusProps{
     id:number;
@@ -29,20 +17,13 @@ const Dashboard = () => {
     status: number;
     description:string
   }
-  
 
   return (
     <div className='dashboard'>
       <Row>
-        {status.map((s:statusProps)=>
-        <Column 
-          key={s.id} 
-          title={s.value} 
-          count={tickets.filter((t:ticketProps)=>s.id === t.status).length}
-        >          
-          {tickets.length > 0 && tickets.filter((t:ticketProps)=>s.id === t.status).map((ticket:ticketProps)=> <Card key={ticket.id} {...ticket} />)}
-        </Column>
-        )}
+        {isLoading && <div>loading...</div>}
+        {error && <div>{ error }</div>}
+        {dataStatuses && dataStatuses.map((s:statusProps)=> <List key= {s.id} id={s.id} value={s.value} />)}
       </Row>
     </div>
   )
