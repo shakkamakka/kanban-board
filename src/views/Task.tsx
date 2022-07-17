1b070bd376c2d6707a87d87860950238a7f434bf
cosmickitten <tjankui@gmail.com>
2022-07-17 20:43:09 +0200

    styling

--- src/views/Task.tsx
+++ src/views/Task.tsx
@@ -9,23 +9,21 @@ const Task = () => {
     data:dataTask,
     isLoading,
     error,
-  } = useFetch(`https://my-json-server.typicode.com/shakkamakka/data/tasks/${id}`);
+  } = useFetch(`http://localhost:3000/tasks/${id}`);
 
-
-  console.log(dataTask)
   return (
-  <div className="task">
+  <section className="page task">
     {isLoading && <div>loading...</div>}
     {error && <div>{error}</div>}
     {dataTask && 
-    <section>
+    <div>
       <div className="task__heading">
         <h2>{dataTask.title}</h2>
         <h4>Status:</h4>
       </div>
       <div>{dataTask.description}</div>
-    </section>}
-  </div>   
+    </div>}
+  </section>   
   )
 };
 
