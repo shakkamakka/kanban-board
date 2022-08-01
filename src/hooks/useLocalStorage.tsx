266bb23da52c8ef3b106bff6097cd1d9ff1592fa
cosmickitten <tjankui@gmail.com>
2022-08-01 16:23:17 +0200

    init migration of json-server to localstorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,25 +1,42 @@
-import {FC, useState, useEffect} from 'react'
+import {useState} from 'react'
 
-const getSavedValue = (key :string, initialValue:Function | string) => {
-  const savedValue= localStorage.getItem(key);
-  const parsedValue = savedValue ? JSON.parse(savedValue): "{}";
-  if(parsedValue) return parsedValue;
-
-  if(initialValue instanceof Function) return initialValue();
-  return initialValue;
-
-}
-
-const useLocalStorage = (key :string, initialValue:FC | string) => {
-  const [localValue, setLocalValue] = useState(()=>{
-    return getSavedValue(key, initialValue);
-  })
-
-  useEffect(()=>{
-    localStorage.setItem(key,JSON.stringify(localValue))
-  }, [localValue])
-
-  return [localValue, setLocalValue];
+const useLocalStorage = <T,>(key: string, initialValue: T) =>{
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue] as const;
 }
 
 export default useLocalStorage;
